31a146043032ae638a95a9e2d17064574e71229b
KURNIAWAN SATRIA <satganzdevs@gmail.com>
2026-05-29 02:25:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ if (!editor) throw new Error('Editor not found')
 
 // ==================== SET CLIPBOARD & PASTE ====================
 if (isDebug) console.log(magenta('  [~] Set clipboard & paste pesan...'))
-const message = `[ BOT ] 👍`
+const message = `👍`
 
 await editor.click()
 await new Promise(r => setTimeout(r, 300))
